0a72922e28e6535b808930ffa952564d203f57a3
Paul de Vrieze <paul.devrieze@gmail.com>
2012-07-23 15:21:23 +0000

    Improved layout
    git-svn-id: https://darwin.bournemouth.ac.uk/pdvrieze/svn/darwin/projects/android-auth@64 3bae1aa3-1c52-4c45-a669-1fb9b31e7360

--- src/uk/ac/bournemouth/darwin/auth/DarwinAuthenticatorActivity.java
+++ src/uk/ac/bournemouth/darwin/auth/DarwinAuthenticatorActivity.java
@@ -1,6 +1,7 @@
 package uk.ac.bournemouth.darwin.auth;
 
 import android.accounts.AccountAuthenticatorActivity;
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.Window;
@@ -8,14 +9,28 @@ import android.view.Window;
 
 public class DarwinAuthenticatorActivity extends AccountAuthenticatorActivity {
 
+  static final String PARAM_USERNAME = "username";
+  static final String PARAM_CONFIRM = "confirm";
+  private String aUsername;
+  private boolean aRequestNewAccount;
+  private boolean aConfirmCredentials;
+
   @Override
   protected void onCreate(Bundle pIcicle) {
     super.onCreate(pIcicle);
+    
+    final Intent intent = getIntent();
+    
+    aUsername = intent.getStringExtra(PARAM_USERNAME);
+    aRequestNewAccount = aUsername==null;
+    aConfirmCredentials = intent.getBooleanExtra(PARAM_CONFIRM, false);
     
     if (Build.VERSION.SDK_INT<11) { // No actionbar
       requestWindowFeature(Window.FEATURE_LEFT_ICON);
     }
     setContentView(R.layout.get_password);
+    
+    
     
   }
 
