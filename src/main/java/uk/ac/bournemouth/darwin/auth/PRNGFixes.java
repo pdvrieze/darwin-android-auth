33862872ac20e3b709c85614046317934b4f61f2
Paul de Vrieze <paul.devrieze@gmail.com>
2016-01-08 20:19:37 +0000

    Add copyright notices to all files

--- src/main/java/uk/ac/bournemouth/darwin/auth/PRNGFixes.java
+++ src/main/java/uk/ac/bournemouth/darwin/auth/PRNGFixes.java
@@ -1,4 +1,20 @@
 /*
+ * Copyright (c) 2016.
+ *
+ * This file is part of ProcessManager.
+ *
+ * ProcessManager is free software: you can redistribute it and/or modify it under the terms of version 2.1 of the
+ * GNU Lesser General Public License as published by the Free Software Foundation.
+ *
+ * ProcessManager is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+ * the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with Foobar.  If not,
+ * see <http://www.gnu.org/licenses/>.
+ */
+
+/*
  * This software is provided 'as-is', without any express or implied
  * warranty.  In no event will Google be held liable for any damages
  * arising from the use of this software.
