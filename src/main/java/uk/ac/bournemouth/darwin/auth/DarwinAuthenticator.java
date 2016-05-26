5ff0349a670020b3d3630aa159859fcdab08b20a
Paul de Vrieze <paul.devrieze@gmail.com>
2016-05-26 11:31:43 +0100

    Updated debugging information.

--- src/main/java/uk/ac/bournemouth/darwin/auth/DarwinAuthenticator.java
+++ src/main/java/uk/ac/bournemouth/darwin/auth/DarwinAuthenticator.java
@@ -585,7 +585,7 @@ public class DarwinAuthenticator extends AbstractAccountAuthenticator {
 
   @Override
   public Bundle hasFeatures(final AccountAuthenticatorResponse response, final Account account, final String[] features) throws NetworkErrorException {
-    Log.i(TAG, "hasFeatures() called with: " + "response = [" + response + "], account = [" + account + "], features = [" + Arrays.toString(features) + "]");
+    Log.i(TAG, "hasFeatures() called with: " + "response = [" + response + "], account = [" + account + "], features = " + Arrays.deepToString(features));
     final boolean hasFeature;
     if (features.length == 1) {
       final AccountManager am = AccountManager.get(mContext);
